a4b56452ea5d085936c71bc6d76a0c5f578f22ef
dinhdjj <dinhdjj@gmail.com>
2021-11-08 09:12:49 +0700

    refactor: move from `own` to `my-account-types` page

--- layouts/primary.vue
+++ layouts/primary.vue
@@ -243,6 +243,29 @@
                         </NuxtLink>
 
                         <NuxtLink
+                          v-if="$auth.can('createAccountType')"
+                          :to="{ name: 'my-account-types-create' }"
+                          class="
+                            group
+                            flex
+                            items-center
+                            py-1.5
+                            hover:text-teal-600
+                          "
+                        >
+                          <IconsPlus
+                            class="
+                              size-xl
+                              flex-none
+                              mr-3
+                              text-gray-400
+                              group-hover:text-indigo-600
+                            "
+                          />
+                          Tạo kiểu tài khoản
+                        </NuxtLink>
+
+                        <NuxtLink
                           :to="{ name: 'accounts-create' }"
                           class="
                             group
@@ -517,6 +540,21 @@
                   "
                 >
                   Quản trị
+                </NuxtLink>
+                <NuxtLink
+                  v-if="$auth.can('createAccountType')"
+                  :to="{ name: 'my-account-types-create' }"
+                  class="
+                    block
+                    px-4
+                    py-2
+                    text-base
+                    font-medium
+                    text-gray-500
+                    hover:text-gray-800 hover:bg-gray-100
+                  "
+                >
+                  Tạo kiểu tài khoản
                 </NuxtLink>
                 <NuxtLink
                   :to="{ name: 'accounts-create' }"
